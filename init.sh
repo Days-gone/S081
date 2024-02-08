7e9a0f37e090ee4b4dced06887004b3a767f2391
xym <1328699220@qq.com>
2024-02-09 00:21:17 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 sudo apt update
 sudo apt upgrade
 sudo apt-get install git build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu
+
+# if you wanna remove the tools, run the bash commands bellow
+# sudo apt remove gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu
+# sudo apt autoremove
