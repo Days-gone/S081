cad5321df7eebb02c3b0acaa5d2684f66c96aeb4
xym <1328699220@qq.com>
2024-03-04 11:53:19 +0800

    finish speedup syscalls

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,14 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall.
+  if ((p->sp_syscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->sp_syscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -160,6 +168,9 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+  if (p->sp_syscall)
+    kfree((void*)p->sp_syscall);
+  p->sp_syscall = 0;
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -202,6 +213,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the usyscall to USYSCALL va. 
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)p->sp_syscall,PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +231,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
